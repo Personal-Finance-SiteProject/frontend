97cbf10bc8527c0f1847b5525969a23a8875cf95
Daniel <dev.danielmarroquin25@gmail.com>
2023-10-05 12:46:28 -0500

    Config template Part #1

--- src/@core/layouts/components/shared-components/footer/FooterContent.tsx
+++ src/@core/layouts/components/shared-components/footer/FooterContent.tsx
@@ -1,51 +1,23 @@
 // ** MUI Imports
 import Box from '@mui/material/Box'
 import Link from '@mui/material/Link'
-import { Theme } from '@mui/material/styles'
 import Typography from '@mui/material/Typography'
-import useMediaQuery from '@mui/material/useMediaQuery'
 
 const FooterContent = () => {
-  // ** Var
-  const hidden = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'))
+  // const hidden = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'))
 
   return (
     <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'space-between' }}>
-      <Typography sx={{ mr: 2 }}>
+      <Typography sx={{ mr: 2, fontSize: '12px' }}>
         {`© ${new Date().getFullYear()}, Made with `}
         <Box component='span' sx={{ color: 'error.main' }}>
           ❤️
         </Box>
         {` by `}
-        <Link target='_blank' href='https://themeselection.com/'>
-          ThemeSelection
+        <Link target='_blank' href='https://www.linkedin.com/in/dmarroqu%C3%ADn-72724a208/' sx={{ fontSize: '12px' }}>
+          Daniel Marroquin
         </Link>
       </Typography>
-      {hidden ? null : (
-        <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', '& :not(:last-child)': { mr: 4 } }}>
-          <Link
-            target='_blank'
-            href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free/blob/main/LICENSE'
-          >
-            MIT License
-          </Link>
-          <Link target='_blank' href='https://themeselection.com/'>
-            More Themes
-          </Link>
-          <Link
-            target='_blank'
-            href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free/blob/main/README.md'
-          >
-            Documentation
-          </Link>
-          <Link
-            target='_blank'
-            href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free/issues'
-          >
-            Support
-          </Link>
-        </Box>
-      )}
     </Box>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -51,23 +51,19 @@ if (themeConfig.routingLoader) {
   })
 }
 
-// ** Configure JSS & ClassName
 const App = (props: ExtendedAppProps) => {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
 
-  // Variables
   const getLayout = Component.getLayout ?? (page => <UserLayout>{page}</UserLayout>)
 
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <title>{`${themeConfig.templateName} - Material Design React Admin Template`}</title>
+        <title>{`App-${themeConfig.templateName}`}</title>
         <meta
           name='description'
-          content={`${themeConfig.templateName} – Material Design React Admin Dashboard Template – is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.`}
+          content={`${themeConfig.templateName}`}
         />
-        <meta name='keywords' content='Material Design, MUI, Admin Template, React Admin Template' />
-        <meta name='viewport' content='initial-scale=1, width=device-width' />
       </Head>
 
       <SettingsProvider>
